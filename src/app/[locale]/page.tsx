0ea7db96e3bdfab03e3c5e1e8e8bcd1d44d6ece3
Michtz <105941997+Michtz@users.noreply.github.com>
2025-08-29 21:23:18 +0200

    removed dot.env

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,6 @@
 'use client';
 import { FC } from 'react';
-import dotenv from 'dotenv';
 import MainContainer from '@/components/containers/MainContainer';
-dotenv.config();
 
 const Home: FC = () => <MainContainer />;
 export default Home;
